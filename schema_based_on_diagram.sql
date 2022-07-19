41b05e5506989d306229b36536e9133a5245205d
Pedro Marques <ppmarq1@gmail.com>
2022-07-19 16:12:51 +0100

    Update queries

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,23 +1,17 @@
-
-
 CREATE TABLE patients (
-id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-name VARCHAR(200),
-date_of_birth date,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(200),
+    date_of_birth date
 );
 
 CREATE TABLE medical_histories (
-    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id int GENERATED ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP,
     patient_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     status VARCHAR(200),
-     CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id),
+    CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
-CREATE TABLE treatments (id INT PRIMARY KEY,type VARCHAR,name VARCHAR);
-CREATE TABLE invoices (id INT PRIMARY KEY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP,medical_history_id INT);
-
-CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL,quantity INT,total_price DECIMAL,invoice_id INT,treatment_id INT);
 CREATE TABLE treatments (id INT PRIMARY KEY, type VARCHAR, name VARCHAR);
 
 CREATE TABLE invoices (
@@ -36,7 +30,6 @@ CREATE TABLE invoice_items (
     invoice_id INT,
     treatment_id INT
 );
-
 
 ALTER TABLE
     medical_histories
