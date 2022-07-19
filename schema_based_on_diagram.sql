7fab664e504751ae9cb2333cfe2e7e10797a003e
Pedro Marques <ppmarq1@gmail.com>
2022-07-19 21:43:44 +0100

    Eliminate duplicated queries

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,24 +18,6 @@ CREATE TABLE treatments (id INT PRIMARY KEY,type VARCHAR,name VARCHAR);
 CREATE TABLE invoices (id INT PRIMARY KEY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP,medical_history_id INT);
 
 CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL,quantity INT,total_price DECIMAL,invoice_id INT,treatment_id INT);
-CREATE TABLE treatments (id INT PRIMARY KEY, type VARCHAR, name VARCHAR);
-
-CREATE TABLE invoices (
-    id INT PRIMARY KEY,
-    total_amount DECIMAL,
-    generated_at TIMESTAMP,
-    payed_at TIMESTAMP,
-    medical_history_id INT
-);
-
-CREATE TABLE invoice_items (
-    id INT PRIMARY KEY,
-    unit_price DECIMAL,
-    quantity INT,
-    total_price DECIMAL,
-    invoice_id INT,
-    treatment_id INT
-);
 
 
 ALTER TABLE
