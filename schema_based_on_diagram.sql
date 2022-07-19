16afbfc65a0b31d009f2569a37528bcf5836645d
Pedro Marques <ppmarq1@gmail.com>
2022-07-19 14:27:24 +0100

    Join tables and add Foreign key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,34 @@
-CREATE TABLE treatments (id INT PRIMARY KEY,type VARCHAR,name VARCHAR);
-CREATE TABLE invoices (id INT PRIMARY KEY,total_amount DECIMAL,generated_at TIMESTAMP,payed_at TIMESTAMP,medical_history_id INT);
+CREATE TABLE treatments (id INT PRIMARY KEY, type VARCHAR, name VARCHAR);
 
-CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL,quantity INT,total_price DECIMAL,invoice_id INT,treatment_id INT);
+CREATE TABLE invoices (
+    id INT PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT
+);
+
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT
+);
+
+
+ALTER TABLE
+    medical_histories
+ADD
+    CONSTRAINT fk_treatmentID FOREIGN KEY(id) REFERENCES treatments(id);
+
+ALTER TABLE
+    invoice_items
+ADD
+    CONSTRAINT fk_invoice FOREIGN KEY(treatment_id) REFERENCES treatments(id);
+
+ALTER TABLE
+    invoice_items
+ADD
+    CONSTRAINT fk_invoiceItems FOREIGN KEY(invoice_id) REFERENCES invoices(id);
